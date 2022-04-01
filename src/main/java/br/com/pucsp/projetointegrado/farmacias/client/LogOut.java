b53a3c9e35abbd1a8bbfc2368510941f9da7704d
Humberto <humbertoodantas@gmail.com>
2022-03-31 23:42:14 -0300

    LogOut updated

--- src/main/java/br/com/pucsp/projetointegrado/farmacias/client/LogOut.java
+++ src/main/java/br/com/pucsp/projetointegrado/farmacias/client/LogOut.java
@@ -1,31 +1,59 @@
 package br.com.pucsp.projetointegrado.farmacias.client;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import br.com.pucsp.projetointegrado.farmacias.client.logout.LogoutDB;
+import br.com.pucsp.projetointegrado.farmacias.db.DB;
 
 public class LogOut {
 	public static String NAME = LogOut.class.getSimpleName();
 	private static Logger LOG = Logger.getLogger(LogOut.class.getName());
 	
-	public boolean logout(Map <String, String> variables, String session) {
+	public boolean logout(Map <String, String> variables, String session, int SESSION_LENGTH) {
 		LOG.entering(NAME, "logout");
 		
 		LogoutDB userFromDb = new LogoutDB();
 		
-		if(session.matches(variables.get("REGEX_EMAIL"))) {
-			String sql = variables.get("LOGOUT_1");
-			boolean check = userFromDb.LogoutUser(variables, sql, session);
+		if(session.toLowerCase().matches(variables.get("REGEX_EMAIL"))) {
+			String sql1 = "SELECT id_usuario FROM Usuario WHERE (email LIKE ?);";
+			String sql2 = "SELECT id_session FROM Login_Sessao WHERE (id_usuario LIKE ?);";
 			
-			if(check) { 
-				LOG.exiting(NAME, "logout");
-				return true; 
+			try {
+				PreparedStatement statement1 = DB.connect(variables).prepareStatement(sql1);
+				statement1.setString(1, session.toLowerCase());
+				
+				String userId = "";
+				ResultSet g = statement1.executeQuery();
+				while(g.next()) {
+					userId = g.getString(1);
+				}
+				
+				PreparedStatement statement2 = DB.connect(variables).prepareStatement(sql2);
+				statement2.setString(1, userId);
+				
+				String sessionId = "";
+				ResultSet h = statement2.executeQuery();
+				while(h.next()) {
+					sessionId = h.getString(1);
+				}
+				
+				boolean check = userFromDb.LogoutUser(variables, sessionId);
+				
+				if(check) { 
+					LOG.exiting(NAME, "logout");
+					return true; 
+				}
+			} catch (SQLException e) {
+				LOG.log(Level.SEVERE, "LogOut.logout Error:: " + e);
 			}
 		}
-		else if(session.length() > 10 && session.length() < 250) {
-			String sql = variables.get("LOGOUT_2");
-			boolean check = userFromDb.LogoutUser(variables, sql, session);
+		else if(session.length() == SESSION_LENGTH) {
+			boolean check = userFromDb.LogoutUser(variables, session);
 						
 			if(check) {
 				LOG.exiting(NAME, "logout");
