c57a76a988ddd9c9e3bf2ca9a09745aabb2b6e23
Humberto <humbertoodantas@gmail.com>
2022-04-04 00:49:08 -0300

    Calculate the delivery fee

--- src/main/java/br/com/pucsp/projetointegrado/farmacias/utils/CalcFeeDelivery.java
+++ src/main/java/br/com/pucsp/projetointegrado/farmacias/utils/CalcFeeDelivery.java
@@ -2,8 +2,8 @@ package br.com.pucsp.projetointegrado.farmacias.utils;
 
 public class CalcFeeDelivery {
 	public static String calcFeeDelivery(double distance) {
-		double min = 6.00;
-		double perKm = 0.99;
+		double min = 4.00;
+		double perKm = 0.79;
 		
 		double fee = (perKm*distance)+min;
 		
