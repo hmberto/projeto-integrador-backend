1ae9b611ae03d64def162726560b5be279de9bee
Humberto <humbertoodantas@gmail.com>
2022-05-21 01:43:35 -0300

    Pharmacy Order

--- ProjetoIntegradorProducts/src/main/java/br/com/pucsp/projetointegrador/product/products/ProductsDB.java
+++ ProjetoIntegradorProducts/src/main/java/br/com/pucsp/projetointegrador/product/products/ProductsDB.java
@@ -81,8 +81,11 @@ public class ProductsDB {
 			
 			List<String> productsId = new ArrayList<String>();
 			
+			List<String> newProductsList0 = new ArrayList<String>();
+			
 			while(f2.next()) {
 				productsId.add(f2.getString(1));
+				newProductsList0.add("{ id: '" + f2.getString(1) + "', amount: '" + "AMOUNT_" + "', name: '" + "NAME_" + "', pharmacy: 'PHARMACY_NAME', price: 'R$ PRICE_FLOAT', image: '" + "IMAGE_" + "', description: '" + "DESCRIPTION_" + "' }");
 			}
 			
 			StringBuffer stringfyProducts = new StringBuffer();
@@ -98,11 +101,28 @@ public class ProductsDB {
 			List<String> productsSearchIds = new ArrayList<String>();
 			Map<Integer, String> productsSearch = new HashMap<Integer, String>();
 			
+			List<String> newProductsList1 = new ArrayList<String>();
+			
 			while(f3.next()) {
 				int productId = Integer.parseInt(f3.getString(1));
 				productsSearch.put(productId ,"{ id: '" + f3.getString(1) + "', amount: '" + f3.getString(5) + "', name: '" + f3.getString(2) + "', pharmacy: 'PHARMACY_NAME', price: 'R$ PRICE_FLOAT', image: '" + f3.getString(6) + "', description: '" + f3.getString(7) + "' }");
 				productsSearchIds.add(f3.getString(1));
+				
+
+				for(int i = 0; i < newProductsList0.size(); i++) {
+					JSONObject item = new JSONObject(newProductsList0.get(i));
+					
+					if(item.get("id").equals(f3.getString(1))) {
+						String tmp0 = newProductsList0.get(i).replace("AMOUNT_", f3.getString(5));
+						String tmp1 = tmp0.replace("NAME_", f3.getString(2));
+						String tmp2 = tmp1.replace("IMAGE_", f3.getString(6));
+						String tmp3 = tmp2.replace("DESCRIPTION_", f3.getString(7));
+						
+						newProductsList1.add(tmp3);
+					}
+				}
 			}
+			
 			
 			StringBuffer productsSearchId = new StringBuffer();
 			
@@ -119,16 +139,34 @@ public class ProductsDB {
 			
 			Map<Integer, String> finalProducts = new HashMap<Integer, String>();
 			
+			List<String> newProductsList2 = new ArrayList<String>();
+			
 			while(f4.next()) {
 				pharmaciesIds.add(f4.getString(1));
 				ProductsPharmacies.put(f4.getInt(1), f4.getInt(2));
 				String priceProduct = String.format("%.2f", f4.getFloat(3));
+				String pharmacyNumberId = f4.getString(1);
 //				price.put(f4.getInt(2), priceProduct);
 				priceProduct.replace(".", ",");
 				
 				String tmp = productsSearch.get(f4.getInt(2));
 				String tmpProduct = tmp.replace("PRICE_FLOAT", priceProduct);
 				finalProducts.put(f4.getInt(2), tmpProduct);
+				
+				for(int i = 0; i < newProductsList1.size(); i++) {
+					JSONObject item = new JSONObject(newProductsList1.get(i));
+					
+					if(item.get("id").equals(f4.getString(2))) {
+						String tmp0 = newProductsList1.get(i);
+						String tmp1 = tmp0.replace("PRICE_FLOAT", priceProduct);
+						String tmp2 = tmp1.replace("PHARMACY_NAME", "PHARMACY_" + pharmacyNumberId);
+						
+						if(newProductsList2.contains(tmp2)) {}
+						else {
+							newProductsList2.add(tmp2);
+						}
+					}
+				}
 			}
 			
 			StringBuffer stringfyPharmaciesIds = new StringBuffer();
@@ -143,15 +181,49 @@ public class ProductsDB {
 			List<String> products = new ArrayList<String>();
 			List<String> pharmacies = new ArrayList<String>();
 			
+			List<String> newProductsList = new ArrayList<String>();
+			
 			while(f5.next()) {
 				int idProduct = ProductsPharmacies.get(f5.getInt(1));
+				String idProduct2 = ProductsPharmacies.get(f5.getInt(1)) + "";
 				
 				String tmp = finalProducts.get(idProduct);
 				String tmpProduct = tmp.replace("PHARMACY_NAME", f5.getString(2));
 				
 				products.add(tmpProduct);
 				pharmacies.add(f5.getString(2));
+				
+				for(int i = 0; i < newProductsList2.size(); i++) {
+					JSONObject item = new JSONObject(newProductsList2.get(i));
+					
+					System.out.println("\n\n\n");
+					System.out.println(item.get("pharmacy"));
+					System.out.println("\n\n\n");
+					System.out.println(f5.getString(1));
+					System.out.println("\n\n\n");
+					
+					if(item.get("pharmacy").equals("PHARMACY_" + f5.getString(1))) {
+						String tmp0 = newProductsList2.get(i);
+						String tmp1 = tmp0.replace("PHARMACY_" + f5.getString(1), f5.getString(2));
+						System.out.println(tmp1);
+						
+						if(newProductsList.contains(tmp1)) {}
+						else {
+							newProductsList.add(tmp1);
+						}
+					}
+				}
 			}
+			
+			System.out.println("\n\n\n");
+			System.out.println(newProductsList0.size());
+			System.out.println("\n\n\n");
+			System.out.println(newProductsList1.size());
+			System.out.println("\n\n\n");
+			System.out.println(newProductsList2);
+			System.out.println("\n\n\n");
+			System.out.println(newProductsList);
+			System.out.println("\n\n\n");
 			
 			createPayload.object();
 			
@@ -163,8 +235,8 @@ public class ProductsDB {
 				
 				createPayload.object();
 				
-				for(int b = 0; b < products.size(); b ++) {
-					JSONObject item = new JSONObject(products.get(b));
+				for(int b = 0; b < newProductsList.size(); b ++) {
+					JSONObject item = new JSONObject(newProductsList.get(b));
 					if(pharmacyName.equals(item.get("pharmacy"))) {
 						createPayload.key("product-" + (b+1));
 						
